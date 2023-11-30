8d82faff935053f5a10be84d8d265bdb165a2932
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2023-11-30 03:14:55 -0800

    changed folder names for qol when searching in terminal

---
+++ frontend-jobly/src/pages/job/JobList.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+import SearchForm from "../../components/form/SearchForm";
+import JoblyApi from "../../utilities/api";
+import JobCardList from "../../components/job/JobCardList";
+
+/** renders list of all jobs.
+ *
+ * State:
+ * - jobs: [{ id, title, salary, equity }, ...]
+ *
+ * RoutesList -> JobList
+ */
+
+function JobList({ apply }) {
+  const [jobs, setJobs] = useState(null);
+
+  useEffect(() => {
+    async function fetchJobs() {
+      const data = await JoblyApi.getJobs("");
+      setJobs(data);
+    }
+    fetchJobs();
+  }, []);
+
+  /** filter: takes in a search term and
+   * returns a list of jobs that match that title */
+  async function filter(searchTerm) {
+    const data = await JoblyApi.getJobs(searchTerm);
+    setJobs(data);
+  }
+
+  if (!jobs) return <h1>Jobs Loading....</h1>;
+
+  return (
+    <div className="JobList container d-flex flex-column align-items-center">
+      <SearchForm filter={filter} />
+
+      <div className="row row-cols-1">
+        {jobs.length === 0 && <p>Sorry, no results were found!</p>}
+        <JobCardList jobs={jobs} />
+      </div>
+    </div>
+
+  );
+}
+
+export default JobList;
---
+++ frontend-jobly/src/pages/profile/Profile.js
@@ -0,0 +1,48 @@
+import React, { useContext, useState, useEffect } from "react";
+import "./Profile.css";
+
+import ProfileForm from "./ProfileForm";
+import userContext from "../../utilities/userContext";
+import JoblyApi from "../../utilities/api";
+import JobCardList from "../../components/job/JobCardList";
+
+
+/** renders profile page for user.
+ *
+ * Props:
+ * - editProfile(): parent function that saves data that is changed from editing
+ *   profile. -> passed down to ProfileForm.
+ *
+ * Profile -> ProfileForm
+ */
+function Profile({ editProfile }) {
+  const [jobs, setJobs] = useState(null);
+  const { hasAppliedToJob } = useContext(userContext);
+
+  useEffect(() => {
+    async function fetchJobs() {
+      const data = await JoblyApi.getJobs("");
+      const applications = data.filter(j => hasAppliedToJob(j.id));
+      setJobs(applications);
+    }
+    fetchJobs();
+  }, [hasAppliedToJob]);
+
+  if (!jobs) return <h1>Profile Loading....</h1>;
+
+  return (
+    <div className="Profile">
+      <h1>Profile</h1>
+      <ProfileForm editProfile={editProfile} />
+
+      <div className="row row-cols-1">
+        <h3>Jobs Applied:</h3>
+        {jobs.length === 0 && <p>Sorry, no job applications found.</p>}
+        <JobCardList jobs={jobs} />
+      </div>
+
+    </div>
+  );
+}
+
+export default Profile;
